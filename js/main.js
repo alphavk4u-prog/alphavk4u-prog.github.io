dffff632f87f54423aabf81955ddb328ed4e4802
alphavk4u-prog <alphavk4u@gmail.com>
2025-12-28 13:42:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-function copyPrompt() {
-  const text = document.getElementById("promptText");
+function copyPrompt(id) {
+  const text = document.getElementById(id);
   text.select();
+  text.setSelectionRange(0, 99999);
   document.execCommand("copy");
   alert("Prompt copied ✅");
 }
